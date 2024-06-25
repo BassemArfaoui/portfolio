97f837cf35a3ee6636a07b29e8aba3559fc2950b
BassemArfaoui <bassem.arfaoui@insat.ucar.tn>
2024-06-25 23:25:35 +0100

    portfolio

--- portfolio.js
+++ portfolio.js
@@ -10,21 +10,26 @@ const descriptions = [
   "Reviews website built with PostgreSQL-Express-React-Node",
 ];
 
+const sources = [
+  "./test.html",
+  "./test.html",
+  "./test.html",
+  "./test.html",
+  "./test.html",
+  "./test.html",
+  "./test.html",
+  "./test.html",
+];
 
-const sources=['./test.html','./test.html','./test.html','./test.html','./test.html','./test.html','./test.html','./test.html'];
-
-const project_images=document.querySelectorAll('.slider');
-for(let i=0 ; i<sources.length ;i++)
-{
-  project_images[i].addEventListener('click', ()=> 
-  {
+const project_images = document.querySelectorAll(".slider");
+for (let i = 0; i < sources.length; i++) {
+  project_images[i].addEventListener("click", () => {
     window.open(sources[i]);
-  })
-};
+  });
+}
 
 const sliders = document.querySelectorAll(".slider");
 const textElement = document.querySelector(".text");
-
 
 for (let i = 0; i < sliders.length; i++) {
   sliders[i].addEventListener("mouseover", () => {
@@ -37,7 +42,6 @@ for (let i = 0; i < sliders.length; i++) {
     textElement.textContent = "";
   });
 }
-
 
 //scrolling
 document.getElementById("buttonScroll").addEventListener("click", (event) => {
@@ -52,20 +56,17 @@ const hidden = document.querySelector(".hide");
 const contact = document.querySelector(".contact");
 const elements = document.getElementsByTagName("i");
 
-const title=document.querySelector('.hero');
+const title = document.querySelector(".hero");
 //media query
-if (window.matchMedia("(max-width: 1000px)").matches) 
-{
-    hidden.style.setProperty("display", "none", "important");
-    title.style.setProperty("margin-top", "100px", "important");}
-
-
-
-function changeDisplay() {
-    hidden.style.setProperty("display", "none", "important");
-    hidden.classList.remove("fade-out");
+if (window.matchMedia("(max-width: 1000px)").matches) {
+  hidden.style.setProperty("display", "none", "important");
+  title.style.setProperty("margin-top", "100px", "important");
 }
 
+function changeDisplay() {
+  hidden.style.setProperty("display", "none", "important");
+  hidden.classList.remove("fade-out");
+}
 
 contact.addEventListener("click", function (event) {
   event.preventDefault();
@@ -73,59 +74,38 @@ contact.addEventListener("click", function (event) {
   if (hidden.style.display == "none") {
     hidden.style.setProperty("display", "flex", "important");
   } else {
-
     hidden.classList.add("fade-out");
-    setTimeout(changeDisplay,300);
+    setTimeout(changeDisplay, 300);
   }
 });
 
 window.addEventListener("scroll", () => {
-  if(window.scrollY > 150)  contact.style.opacity='0.7';
+  if (window.scrollY > 150) contact.style.opacity = "0.7";
   if (hidden.style.display != "none" && window.scrollY > 150) {
     hidden.classList.add("fade-out");
-    setTimeout(changeDisplay,300)  }
-  if (window.scrollY  ===0) {
+    setTimeout(changeDisplay, 300);
+  }
+  if (window.scrollY === 0) {
     hidden.style.setProperty("display", "flex", "important");
-    if (window.matchMedia("(max-width: 1000px)").matches) 
-    {
+    if (window.matchMedia("(max-width: 1000px)").matches) {
       hidden.style.setProperty("display", "none", "important");
     }
-    contact.style.opacity='1';
+    contact.style.opacity = "1";
   }
 });
 
+contact.addEventListener("mouseover", () => {
+  contact.style.opacity = "1";
+});
 
-contact.addEventListener("mouseover",()=>{
-    contact.style.opacity='1';
-})
-
-
-contact.addEventListener("mouseout",()=>{
-    contact.style.opacity='0.7';
-})
-
-
+contact.addEventListener("mouseout", () => {
+  contact.style.opacity = "0.7";
+});
 
 //small devices
 
-
 const github = document.querySelector(".gitLogo");
 window.addEventListener("scroll", () => {
-  if(window.scrollY > 150) github.style.opacity='0.7';
-  if(window.scrollY === 0) github.style.opacity='1';
-
+  if (window.scrollY > 150) github.style.opacity = "0.7";
+  if (window.scrollY === 0) github.style.opacity = "1";
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
